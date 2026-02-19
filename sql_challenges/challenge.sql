1836c948ecab06a0f94f132ba9da0de7d4395225
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-02-19 09:02:50 -0600

    COMPLETED CLASS

---
+++ sql_challenges/challenge.sql
@@ -0,0 +1,33 @@
+-- 6
+
+SELECT Movies.Title, Boxoffice.domestic_sales, Boxoffice.international_sales
+    FROM movies 
+        INNER JOIN Boxoffice ON Id = Movie_id
+        ORDER BY Id ASC; 
+
+
+SELECT Movies.Title, Boxoffice.domestic_sales, Boxoffice.international_sales
+    FROM movies 
+        INNER JOIN Boxoffice ON Id = Movie_id
+        WHERE Domestic_sales < International_sales; 
+
+SELECT Movies.Title, Boxoffice.domestic_sales, Boxoffice.international_sales
+    FROM movies 
+        INNER JOIN Boxoffice ON Id = Movie_id
+        ORDER BY Rating DESC; 
+
+--- 7
+
+SELECT DISTINCT Building FROM employees;
+
+SELECT * FROM Buildings;
+
+SELECT DISTINCT Building_name , Role FROM Buildings
+    LEFT JOIN Employees ON Building_name = Building;
+
+
+-- INTERVIEW
+SELECT pages.page_id FROM pages
+  LEFT JOIN page_likes ON page_likes.page_id = pages.page_id
+  WHERE page_likes.page_id IS NULL
+  ORDER BY pages.page_id ASC;
