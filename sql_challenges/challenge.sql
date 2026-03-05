ede200c18f9449f4904960bbdb410320c4961c61
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-03-05 07:56:22 -0600

    fixed format

--- sql_challenges/challenge.sql
+++ sql_challenges/challenge.sql
@@ -1,5 +1,13 @@
--- 6
+### `sql_challenges/challenge-01/README.md`
 
+```md
+# SQL Challenge 01 - SQLBot pt.1
+
+## Problem
+Given a table with 10M rows, improve query performance.
+
+## Schema
+```sql
 SELECT Movies.Title, Boxoffice.domestic_sales, Boxoffice.international_sales
     FROM movies 
         INNER JOIN Boxoffice ON Id = Movie_id
@@ -16,7 +24,14 @@ SELECT Movies.Title, Boxoffice.domestic_sales, Boxoffice.international_sales
         INNER JOIN Boxoffice ON Id = Movie_id
         ORDER BY Rating DESC; 
 
---- 7
+```md
+# SQL Challenge 02 - SQLBot pt.2
+
+## Problem
+Given a table with 10M rows, improve query performance.
+
+## Schema
+```sql
 
 SELECT DISTINCT Building FROM employees;
 
@@ -25,8 +40,14 @@ SELECT * FROM Buildings;
 SELECT DISTINCT Building_name , Role FROM Buildings
     LEFT JOIN Employees ON Building_name = Building;
 
+```md
+# SQL Challenge 03 - Interview
 
--- INTERVIEW
+## Problem
+Given a table with 10M rows, improve query performance.
+
+## Schema
+```sql
 SELECT pages.page_id FROM pages
   LEFT JOIN page_likes ON page_likes.page_id = pages.page_id
   WHERE page_likes.page_id IS NULL
