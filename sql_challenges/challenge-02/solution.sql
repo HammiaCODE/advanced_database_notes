92bd9f5131f71785db5c60e8343fb793ecf1aae0
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-03-12 07:10:44 -0600

    Update and rename challenge.sql to solution.sql

--- sql_challenges/challenge-02/solution.sql
+++ sql_challenges/challenge-02/solution.sql
@@ -29,4 +29,5 @@ SELECT DISTINCT Building_name , Role FROM Buildings
 SELECT pages.page_id FROM pages
   LEFT JOIN page_likes ON page_likes.page_id = pages.page_id
   WHERE page_likes.page_id IS NULL
+
   ORDER BY pages.page_id ASC;
